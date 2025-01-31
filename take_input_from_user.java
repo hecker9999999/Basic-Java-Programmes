d7a57870da52cde63d1a9ccbf722e0a1c7e56b0d
hecker99999999 <137152417+hecker9999999@users.noreply.github.com>
2025-01-31 08:28:06 +0000

    seccond commit

--- take_input_from_user.java
+++ take_input_from_user.java
@@ -1,3 +1,25 @@
+
+// ********************Best practice**********************
+
+import java.util.Scanner;
+
+public class take_input_from_user {
+    public static void main(String[] args) {
+        int number;
+        System.out.println("enter a number (only intiger since we are using 'int' datatype) : ");
+        try (Scanner s = new Scanner(System.in)) {
+            number = s.nextInt();
+            System.out.println("your entered number is : " + number);
+            // closing the Scanner object
+            // this is to prevent resource leaks
+        }
+    }
+}
+
+// can also be used
+
+/*  
+
 import java.util.Scanner;
 
 public class take_input_from_user {
@@ -13,3 +35,5 @@ public class take_input_from_user {
         s.close();   // this is to prevent resource leaks
     }
 }
+    
+    */
